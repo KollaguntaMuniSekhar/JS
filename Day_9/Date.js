e092c7dc7cd851216e47fa008e44cd268d96a3cb
KollaguntaMuniSekhar <munisekhark11@gmail.com>
2025-04-07 20:36:23 +0530

    InBuilt Js

---
+++ Day_9/Date.js
@@ -0,0 +1,11 @@
+//dateObject
+// let currentDate = new Date();
+// console.log(currentDate);
+let curr=new Date();
+let date =new Date('june 20 1998 08:15');
+let newDate =new Date(1988,10,11,7);//0- means january 1 -means feb
+console.log(newDate);
+console.log(newDate.getDay());//sunday-0
+console.log(newDate.getFullYear());
+console.log(newDate.setFullYear(2005));
+console.log(newDate);
---
+++ Day_9/InBuilt.js
@@ -0,0 +1,12 @@
+//Math Object
+console.log(Math.PI);
+console.log(Math.max(20,1,2,34,56,78,9));
+console.log(Math.min(20,1,2,34,56,78,9));
+console.log(Math.round(1.6));// Near to that figure like 1.1 that means 1
+console.log(Math.floor(1.9));//min i.e,1
+console.log(Math.ceil(1.1));//max i.e, o/p:2
+console.log(Math.abs(5));
+console.log(Math.abs(-5));//Minus coverted into Plus
+console.log(Math.random());//Prints Random Value
+console.log(Math.sqrt(5));//square root of five 
+console.log(Math.pow(2,3));//two power Three
